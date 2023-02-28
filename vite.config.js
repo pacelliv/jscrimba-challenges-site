8d54e6b6fd3d3db91909337755e0042587785af9
PacelliV <flores.eugenio03@gmail.com>
2023-02-28 13:44:49 -0500

    added outDir path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { resolve } from "path"
 
 const root = resolve(__dirname, "src")
+const outDir = resolve(__dirname, "dist")
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         outDir,
+        emptyOutDir: true,
         rollupOptions: {
             input: {
                 main: resolve(root, "index.html"),
